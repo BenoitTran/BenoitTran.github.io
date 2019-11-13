4cf8f0be9a072499a281ff2685b2a8006fcd400e
Tran <btran.maths@gmail.com>
2019-11-13 03:35:36 +0100

    New code project, Tropical Dynamic Programming, using a Min-Plus algorithm to generate upper approximations and SDDP (coded from scratch to fit the specifities of TDP) to generated lower approximations, of the value functions of a discrete time linear-polyhedral dynamical system with finite support noises.

---
+++ Tropical_Dynamic_Programming/TDP/Eval_Minplus.jl
@@ -0,0 +1,13 @@
+function Eval_Minplus(t, F_up, state)
+"""
+Input:
+t, a time step
+F_up, an array of length T, which contains at each
+component a slope and a value at the origin.
+state, a vector.
+
+Output:
+The evaluation of the supremum of the cuts contained in F_up at the given state.
+"""
+
+end
